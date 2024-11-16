e09d106f2d3b0cce5cfa1ec7a6c5c227dd7d7adf
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-11-16 22:08:23 +0600

    tasklist animation added

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,32 +1,32 @@
-// import PropTypes from 'prop-types';
-// import { CSSTransition, TransitionGroup } from 'react-transition-group';
-// import TaskItem from './TaskItem';
-// import './animations.css';
+import PropTypes from 'prop-types';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import TaskItem from './TaskItem';
+import './animations.css';
 
-// function TaskList({ tasks, deleteTask, toggleComplete }) {
-//     return (
-//         <TransitionGroup className="task-list space-y-4">
-//             {tasks.map(task => (
-//                 <CSSTransition
-//                     key={task.id}
-//                     timeout={500}
-//                     classNames="task"
-//                 >
-//                     <TaskItem
-//                         task={task}
-//                         deleteTask={deleteTask}
-//                         toggleComplete={toggleComplete}
-//                     />
-//                 </CSSTransition>
-//             ))}
-//         </TransitionGroup>
-//     );
-// }
+function TaskList({ tasks, deleteTask, toggleComplete }) {
+    return (
+        <TransitionGroup className="task-list space-y-4">
+            {tasks.map(task => (
+                <CSSTransition
+                    key={task.id}
+                    timeout={500}
+                    classNames="task"
+                >
+                    <TaskItem
+                        task={task}
+                        deleteTask={deleteTask}
+                        toggleComplete={toggleComplete}
+                    />
+                </CSSTransition>
+            ))}
+        </TransitionGroup>
+    );
+}
 
-// TaskList.propTypes = {
-//     tasks: PropTypes.array.isRequired,
-//     deleteTask: PropTypes.func.isRequired,
-//     toggleComplete: PropTypes.func.isRequired
-// };
+TaskList.propTypes = {
+    tasks: PropTypes.array.isRequired,
+    deleteTask: PropTypes.func.isRequired,
+    toggleComplete: PropTypes.func.isRequired
+};
 
-// export default TaskList;
+export default TaskList;
