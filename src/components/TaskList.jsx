67f5c77e58c5333a48ae56bad97ddeddbb14f3e3
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2024-11-16 22:34:56 +0600

    no tasks available implemented if no data exist

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -4,29 +4,34 @@ import TaskItem from './TaskItem';
 import './animations.css';
 
 function TaskList({ tasks, deleteTask, toggleComplete }) {
-    return (
-        <TransitionGroup className="task-list space-y-4">
-            {tasks.map(task => (
-                <CSSTransition
-                    key={task.id}
-                    timeout={500}
-                    classNames="task"
-                >
-                    <TaskItem
-                        task={task}
-                        deleteTask={deleteTask}
-                        toggleComplete={toggleComplete}
-                    />
-                </CSSTransition>
-            ))}
-        </TransitionGroup>
-    );
+  if (tasks.length === 0) {
+    return <div className="text-center text-gray-500 mt-4">
+      No tasks available
+    </div>
+  }
+  return (
+    <TransitionGroup className="task-list space-y-4">
+      {tasks.map(task => (
+        <CSSTransition
+          key={task.id}
+          timeout={500}
+          classNames="task"
+        >
+          <TaskItem
+            task={task}
+            deleteTask={deleteTask}
+            toggleComplete={toggleComplete}
+          />
+        </CSSTransition>
+      ))}
+    </TransitionGroup>
+  );
 }
 
 TaskList.propTypes = {
-    tasks: PropTypes.array.isRequired,
-    deleteTask: PropTypes.func.isRequired,
-    toggleComplete: PropTypes.func.isRequired
+  tasks: PropTypes.array.isRequired,
+  deleteTask: PropTypes.func.isRequired,
+  toggleComplete: PropTypes.func.isRequired
 };
 
 export default TaskList;
